f0eb83cb6eb9376bfe9cfb3725ee27067599b98d
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 00:59:54 +0100

    Handle click on board

--- client/src/Board.tsx
+++ client/src/Board.tsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import { XorO } from "./types";
 
+const PLAYER_X: XorO = "X";
+const PLAYER_Y: XorO = "O";
+
 const Board = () => {
   const [board, setBoard] = useState<(XorO | undefined)[][]>([
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
   ]);
+  const [playerTurn, setPlayerTurn] = useState<XorO>(PLAYER_X);
+
+  const handleClick = (rowIndex: number, colIndex: number) => {
+    const updatedBoard = board.map((row, rowI) => {
+      if (rowI !== rowIndex) return row;
+      return row.map((column, colI) => {
+        if (colI !== colIndex) return column;
+        return column || playerTurn;
+      });
+    });
+    setBoard(updatedBoard);
+  };
 
   return (
     <div className="flex flex-col gap-1">
@@ -14,6 +29,7 @@ const Board = () => {
         <div key={rowIndex} className="flex gap-1">
           {row.map((column, colIndex) => (
             <div
+              onClick={() => handleClick(rowIndex, colIndex)}
               key={colIndex}
               className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex"
             >
