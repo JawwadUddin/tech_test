85732fb8083e0872a0b20a733dac02b0803b1701
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 00:51:55 +0100

    Refactor: create Board component

---
+++ client/src/Board.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { XorO } from "./types";
+
+const Board = () => {
+  const [board, setBoard] = useState<(XorO | undefined)[][]>([
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+  ]);
+
+  return (
+    <div className="flex flex-col gap-1">
+      {board.map((row) => (
+        <div className="flex gap-1">
+          {row.map((column) => (
+            <div className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex">
+              {column}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
