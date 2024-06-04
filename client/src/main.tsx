29a614d21b1f6a8f0c8cd1cb38b59e94e18aa49d
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 01:48:45 +0100

    Add hook call, send props to Board component, add GameOver component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Board from "./Board";
+import GameOver from "./GameOver";
+import useGameState from "./hooks/useGameState";
 
 export const Main = () => {
+  const { gameState, playerTurn, board, handleClick } = useGameState();
   return (
     <div className="flex flex-col mt-10 items-center gap-10">
       <div className="font-bold text-2xl">Tic Tac Toe</div>
-      <Board />
+      <Board
+        board={board}
+        playerTurn={playerTurn}
+        gameState={gameState}
+        handleClick={handleClick}
+      />
+      <GameOver gameState={gameState} />
     </div>
   );
 };
