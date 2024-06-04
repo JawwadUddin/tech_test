3649ba78d821ab8f392c5c65604acb2f67f630b2
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 01:35:04 +0100

    Remove cursor pointer when game not in progress

--- client/src/Board.tsx
+++ client/src/Board.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import useGameState from "./hooks/useGameState";
+import useGameState, { GameState } from "./hooks/useGameState";
 
 const Board = () => {
-  const { playerTurn, board, handleClick } = useGameState();
+  const { playerTurn, board, handleClick, gameState } = useGameState();
 
   return (
     <div className="flex flex-col gap-1">
@@ -12,7 +12,9 @@ const Board = () => {
             <div
               onClick={() => handleClick(rowIndex, colIndex)}
               key={colIndex}
-              className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex"
+              className={`border-2 border-gray-900 w-10 h-10 items-center justify-center text-2xl font-bold flex ${
+                gameState === GameState.inProgress && "cursor-pointer"
+              }`}
             >
               {column}
             </div>
