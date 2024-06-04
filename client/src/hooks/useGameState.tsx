0c35492e22a190f3fdcad48038ab85fe4e0b27fe
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 01:32:16 +0100

    Refactor: move winningCombinations and checkWinner function to utils folder

--- client/src/hooks/useGameState.tsx
+++ client/src/hooks/useGameState.tsx
@@ -1,65 +1,9 @@
 import React, { useState } from "react";
 import { XorO } from "../types";
+import { checkWinner } from "../utils/gameUtils";
 
 const PLAYER_X: XorO = "X";
 const PLAYER_Y: XorO = "O";
-
-let winningCombinations = [
-  // Rows
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-  ],
-  [
-    [1, 0],
-    [1, 1],
-    [1, 2],
-  ],
-  [
-    [2, 0],
-    [2, 1],
-    [2, 2],
-  ],
-  // Columns
-  [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-  ],
-  [
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ],
-  [
-    [0, 2],
-    [1, 2],
-    [2, 2],
-  ],
-  // Diagonals
-  [
-    [0, 0],
-    [1, 1],
-    [2, 2],
-  ],
-  [
-    [0, 2],
-    [1, 1],
-    [2, 0],
-  ],
-];
-
-const checkWinner = (board: (XorO | undefined)[][]) => {
-  for (const combination of winningCombinations) {
-    const [a, b, c] = combination;
-    const valueA = board[a[0]][a[1]];
-    const valueB = board[b[0]][b[1]];
-    const valueC = board[c[0]][c[1]];
-    if (valueA && valueA === valueB && valueA === valueC) return valueA;
-  }
-  return undefined;
-};
 
 export const GameState = {
   playerXWins: 0,
---
+++ client/src/utils/gameUtils.ts
@@ -0,0 +1,58 @@
+import { XorO } from "../types";
+
+let winningCombinations = [
+  // Rows
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  // Columns
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  // Diagonals
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
+
+export const checkWinner = (board: (XorO | undefined)[][]) => {
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    const valueA = board[a[0]][a[1]];
+    const valueB = board[b[0]][b[1]];
+    const valueC = board[c[0]][c[1]];
+    if (valueA && valueA === valueB && valueA === valueC) return valueA;
+  }
+  return undefined;
+};
