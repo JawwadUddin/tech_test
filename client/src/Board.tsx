4413ec2cc71f5ee3b3358ddc13a9537dab3d88a4
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-06-04 01:09:15 +0100

    Add GameState enum, only update tiles when game is is progress

--- client/src/Board.tsx
+++ client/src/Board.tsx
@@ -4,15 +4,23 @@ import { XorO } from "./types";
 const PLAYER_X: XorO = "X";
 const PLAYER_Y: XorO = "O";
 
+const GameState = {
+  playerXWins: 0,
+  playerYWins: 1,
+  draw: 2,
+  inProgress: 3,
+};
+
 const Board = () => {
   const [board, setBoard] = useState<(XorO | undefined)[][]>(
     Array.from({ length: 3 }, () => Array(3).fill(undefined))
   );
   const [playerTurn, setPlayerTurn] = useState<XorO>(PLAYER_X);
+  const [gameState, setGameState] = useState<number>(GameState.inProgress);
 
   const handleClick = (rowIndex: number, colIndex: number) => {
     // do not update tile if value within tile already exists (previously selected)
-    if (board[rowIndex][colIndex]) return;
+    if (board[rowIndex][colIndex] || gameState !== GameState.inProgress) return;
 
     const updatedBoard = board.map((row, rowI) => {
       if (rowI !== rowIndex) return row;
